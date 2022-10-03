9d6de7c03300ca3f5534e233fc4f633ac6f4f798
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-10-03 09:51:36 +0300

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -14,6 +14,15 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
